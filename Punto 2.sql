1c762b22ddc746a45fcadb1bdfb9f8a85812bc73
JuanPTam <jpenat@unal.edu.co>
2021-03-30 13:30:09 -0500

    Se realizaron los literales d y f del punto 2.

--- Punto 2.sql
+++ Punto 2.sql
@@ -3,7 +3,7 @@ CREATE TABLE individuo(
     codigo NUMBER(8) PRIMARY KEY,
     nombre VARCHAR2(20) NOT NULL,
     valor NUMBER(8) NOT NULL CHECK (valor > 0),
-    padre NUMBER(8) , --código del padre del inviduo
+    padre NUMBER(8), --código del padre del inviduo
     nro_hijos NUMBER(8) NOT NULL CHECK (nro_hijos >=0),
     CHECK(padre <> codigo)
 );
@@ -110,7 +110,7 @@ CREATE OR REPLACE TRIGGER trigr3
   BEGIN
   
   IF contador IS NOT NULL THEN
-    UPDATE individuo SET nro_hijos = nro_hijos - numero_filas WHERE codigo = contador; 
+    UPDATE individuo SET nro_hijos = nro_hijos - numero_filas WHERE codigo = contador;
   END IF;
   
   
@@ -124,58 +124,46 @@ END trigr3;
 /
 
 -- d) cuando un individuo se borra y tiene hijos entonces a sus hijos se les pone el atributo padre = null
-
 CREATE OR REPLACE TRIGGER trigr4
-  FOR DELETE  ON individuo COMPOUND TRIGGER
--- Sección declaratica (optional)
--- Variables se declaran durante el Trigger
-
+FOR DELETE ON individuo COMPOUND TRIGGER
   
+  codInd individuo.codigo%TYPE := 0;
 
-  
- numero_filas number(8) := 0;
-
-  
---Ejecución antes de una consulta DML
-  BEFORE STATEMENT IS
-  BEGIN
-  NULL;
-  END BEFORE STATEMENT;
-
--- Ejecución antes de cada fila, variables :NEW, :OLD son permitidas
   BEFORE EACH ROW IS
   BEGIN
-  NULL;
+    codInd := :OLD.codigo;
   END BEFORE EACH ROW;
 
--- Ejecución despues de cada fila, variables :NEW, :OLD son permitidas
-  AFTER EACH ROW IS
-  
-  
-  BEGIN
-  numero_filas :=  numero_filas +1;
-  DBMS_OUTPUT.PUT_LINE(numero_filas);
-  END AFTER EACH ROW;
-
---Ejecución despues de una consulta DML
   AFTER STATEMENT IS
-  contador1 number(4) := :OLD.codigo;
-  
   BEGIN
-  
-  IF contador1 IS NOT NULL THEN
-    UPDATE individuo SET padre = NULL WHERE padre = contador1; 
-  END IF;
-  
-  
-   
+    FOR ind IN (SELECT * FROM INDIVIDUO WHERE padre = codInd) LOOP
+      UPDATE individuo SET padre = Null WHERE codigo = ind.codigo;
+    END LOOP;
   END AFTER STATEMENT;
-  
-
-
- 
 END trigr4;
 /
+
+-- f) cuando se actualice un código, se debe actualizar ese código también en sus hijos
+CREATE OR REPLACE TRIGGER trigr6
+FOR UPDATE OF codigo ON individuo COMPOUND TRIGGER
+  oldCod individuo.codigo%TYPE;
+  newCod individuo.codigo%TYPE;
+
+  BEFORE EACH ROW IS
+  BEGIN
+    oldCod := :OLD.codigo;
+    newCod := :NEW.codigo;
+  END BEFORE EACH ROW;
+
+  AFTER STATEMENT IS
+  BEGIN
+    FOR ind IN (SELECT * FROM INDIVIDUO WHERE padre = oldCod) LOOP
+      UPDATE individuo SET padre = newCod WHERE codigo = ind.codigo;
+    END LOOP;
+  END AFTER STATEMENT;
+END trigr6;
+
+
 
 DELETE individuo;
 --INSERT INTO individuo VALUES(1,'juan peña',1000,NULL,1);
@@ -192,7 +180,8 @@ INSERT INTO individuo VALUES(125,'Amanda Marshall',32,19,0);
 
 INSERT INTO individuo VALUES(123,'Amanda Marshall',32,19,23);
 
-DELETE FROM individuo WHERE valor > 30;
+DELETE FROM individuo WHERE codigo = 19; -- Para probar el d
+UPDATE individuo SET codigo = 20 WHERE codigo = 19; -- Para probar el f
 
 
 DROP TRIGGER JASON.trigr3;
